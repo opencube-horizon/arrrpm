2ddd1becb6b54fea699bdfe6026b5ef03b3219ce
Tiziano Müller <tiziano.mueller@hpe.com>
2024-03-13 15:03:31 +0100

    arrrpm: fix conventions, make ls work with multiple pkgs

--- arrrpm/src/main.rs
+++ arrrpm/src/main.rs
@@ -1,11 +1,11 @@
 use argh::FromArgs;
-use std::collections::BTreeMap;
-use petgraph::graph::Graph;
-use petgraph::dot::{Dot, Config};
-use rpm::{Package, PackageMetadata, FileMode};
-use regex::RegexSet;
 use indent::indent_all_by;
+use petgraph::dot::{Config, Dot};
+use petgraph::graph::Graph;
 use phf::phf_map;
+use regex::RegexSet;
+use rpm::{FileMode, Package, PackageMetadata};
+use std::collections::BTreeMap;
 
 #[derive(FromArgs)]
 /// Some RPM tools
@@ -37,12 +37,12 @@ struct SubcommandDepTree {
 }
 
 #[derive(FromArgs)]
-/// List files in the given RPM
+/// List files in the given RPM(s)
 #[argh(subcommand, name = "ls")]
 struct SubcommandLs {
-    /// RPM
+    /// list of RPMs
     #[argh(positional)]
-    rpm: String,
+    rpms: Vec<String>,
 }
 
 #[derive(FromArgs)]
@@ -67,7 +67,8 @@ struct SubcommandCat {
     scriptlets: Vec<String>,
 }
 
-static SCRIPTLET_METHODS: phf::Map<&'static str, fn(&PackageMetadata) -> Result<rpm::Scriptlet, rpm::Error>> = phf_map! {
+type ScriptletMethod = fn(&PackageMetadata) -> Result<rpm::Scriptlet, rpm::Error>;
+static SCRIPTLET_METHODS: phf::Map<&'static str, ScriptletMethod> = phf_map! {
     "pre_install" => PackageMetadata::get_pre_install_script,
     "post_install" => PackageMetadata::get_post_install_script,
     "pre_uninstall" => PackageMetadata::get_pre_uninstall_script,
@@ -83,7 +84,6 @@ fn main() {
 
     match arrrpm.subcommand {
         ArrrpmSubcommands::DepTree(cmd) => {
-
             let excludes = match RegexSet::new(&cmd.exclude) {
                 Ok(excludes) => excludes,
                 Err(error) => {
@@ -95,7 +95,7 @@ fn main() {
             let mut tree: Graph<String, String, petgraph::Directed> = Graph::new();
 
             for rpm in cmd.rpms.iter() {
-                let pkg = match PackageMetadata::open(&rpm) {
+                let pkg = match PackageMetadata::open(rpm) {
                     Ok(pkg) => pkg,
                     Err(error) => {
                         eprintln!("Opening {:#?} failed: {:#?}, ignoring...", rpm, error);
@@ -106,7 +106,10 @@ fn main() {
                 let name = match pkg.get_name() {
                     Ok(name) => name.to_string(),
                     Err(error) => {
-                        eprintln!("Reading name for {:#?} failed: {:#?}, ignoring...", rpm, error);
+                        eprintln!(
+                            "Reading name for {:#?} failed: {:#?}, ignoring...",
+                            rpm, error
+                        );
                         continue;
                     }
                 };
@@ -118,86 +121,115 @@ fn main() {
                 let reqs = match pkg.get_requires() {
                     Ok(reqs) => reqs,
                     Err(error) => {
-                        eprintln!("Reading requirements for {:#?} failed: {:#?}, ignoring...", rpm, error);
+                        eprintln!(
+                            "Reading requirements for {:#?} failed: {:#?}, ignoring...",
+                            rpm, error
+                        );
                         continue;
                     }
                 };
                 let recs = match pkg.get_recommends() {
                     Ok(recs) => recs,
                     Err(error) => {
-                        eprintln!("Reading requirements for {:#?} failed: {:#?}, ignoring...", rpm, error);
+                        eprintln!(
+                            "Reading requirements for {:#?} failed: {:#?}, ignoring...",
+                            rpm, error
+                        );
                         continue;
                     }
                 };
 
                 // add node for the package itself if it does not exist yet and get index for/from graph
-                let parent_idx = *pkgs.entry(name).or_insert_with_key(|k| tree.add_node(k.clone()));
+                let parent_idx = *pkgs
+                    .entry(name)
+                    .or_insert_with_key(|k| tree.add_node(k.clone()));
 
                 for dep in reqs.iter() {
-                    if dep.name.contains("(") || dep.name.starts_with("/") { // better to use the flags here?!
+                    if dep.name.contains('(') || dep.name.starts_with('/') {
+                        // better to use the flags here?!
                         continue;
                     }
                     if excludes.is_match(&dep.name) {
                         continue;
                     }
 
-                    let dep_idx = *pkgs.entry(dep.name.clone()).or_insert_with_key(|k| tree.add_node(k.clone()));
+                    let dep_idx = *pkgs
+                        .entry(dep.name.clone())
+                        .or_insert_with_key(|k| tree.add_node(k.clone()));
                     tree.add_edge(parent_idx, dep_idx, String::from("required"));
                 }
 
                 for dep in recs.iter() {
-                    if dep.name.contains("(") || dep.name.starts_with("/") { // better to use the flags here?!
+                    if dep.name.contains('(') || dep.name.starts_with('/') {
+                        // better to use the flags here?!
                         continue;
                     }
                     if excludes.is_match(&dep.name) {
                         continue;
                     }
 
-                    let dep_idx = *pkgs.entry(dep.name.clone()).or_insert_with_key(|k| tree.add_node(k.clone()));
+                    let dep_idx = *pkgs
+                        .entry(dep.name.clone())
+                        .or_insert_with_key(|k| tree.add_node(k.clone()));
                     tree.add_edge(parent_idx, dep_idx, String::from("recommended"));
                 }
             }
 
-            println!("{:?}", Dot::with_attr_getters(&tree,
-                                                    &[Config::NodeNoLabel, Config::EdgeNoLabel],
-                                                    &|_, er| format!("label = \"{}\"", er.weight()),
-                                                    &|_, nr| format!("label = \"{}\"", nr.1),
-                                                    ));
-        },
+            println!(
+                "{:?}",
+                Dot::with_attr_getters(
+                    &tree,
+                    &[Config::NodeNoLabel, Config::EdgeNoLabel],
+                    &|_, er| format!("label = \"{}\"", er.weight()),
+                    &|_, nr| format!("label = \"{}\"", nr.1),
+                )
+            );
+        }
 
         ArrrpmSubcommands::Ls(cmd) => {
-            let pkg = match PackageMetadata::open(&cmd.rpm) {
-                Ok(pkg) => pkg,
-                Err(error) => {
-                    panic!("Opening {:#?} failed: {:#?}", cmd.rpm, error);
-                }
-            };
+            for rpm in cmd.rpms.iter() {
+                let pkg = match PackageMetadata::open(rpm) {
+                    Ok(pkg) => pkg,
+                    Err(error) => {
+                        panic!("Opening {:#?} failed: {:#?}", rpm, error);
+                    }
+                };
 
-            let fentries = match pkg.get_file_entries() {
-                Ok(fentries) => fentries,
-                Err(error) => {
-                    panic!("Reading requirements for {:#?} failed: {:#?}", cmd.rpm, error);
+                if cmd.rpms.len() > 1 {
+                    println!("{}:", pkg.get_name().unwrap_or("INVALID"))
                 }
-            };
-            
-            for fentry in fentries.iter() {
-                match fentry.mode {
-                    FileMode::Dir { permissions: _ } => {
-                        println!("{}", fentry.path.display());
-                    },
-                    FileMode::Regular { permissions: _ } => {
-                        println!("{}", fentry.path.display());
-                    },
-                    FileMode::SymbolicLink { permissions: _ } => {
-                        println!("{} -> {}", fentry.path.display(), fentry.linkto);
+
+                let fentries = match pkg.get_file_entries() {
+                    Ok(fentries) => fentries,
+                    Err(error) => {
+                        panic!("Reading requirements for {:#?} failed: {:#?}", rpm, error);
                     }
-                    FileMode::Invalid { raw_mode: _, reason } => {
-                        eprintln!("Invalid file entry for: {:?}: {}", fentry.path, reason);
+                };
+
+                let indent = if cmd.rpms.len() > 1 { "  - " } else { "" };
+
+                for fentry in fentries.iter() {
+                    match fentry.mode {
+                        FileMode::Dir { permissions: _ } => {
+                            println!("{}{}", indent, fentry.path.display());
+                        }
+                        FileMode::Regular { permissions: _ } => {
+                            println!("{}{}", indent, fentry.path.display());
+                        }
+                        FileMode::SymbolicLink { permissions: _ } => {
+                            println!("{}{} -> {}", indent, fentry.path.display(), fentry.linkto);
+                        }
+                        FileMode::Invalid {
+                            raw_mode: _,
+                            reason,
+                        } => {
+                            eprintln!("Invalid file entry for: {:?}: {}", fentry.path, reason);
+                        }
+                        _ => todo!(),
                     }
-                    _ => todo!(),
                 }
             }
-        },
+        }
 
         ArrrpmSubcommands::Info(cmd) => {
             let pkg = match PackageMetadata::open(&cmd.rpm) {
@@ -213,17 +245,16 @@ fn main() {
             println!("Vendor: {}", pkg.get_vendor().unwrap_or(""));
             println!("Packager: {}", pkg.get_packager().unwrap_or(""));
 
-            println!("Description: |\n{}", indent_all_by(2, pkg.get_description().unwrap_or("")));
+            println!(
+                "Description: |\n{}",
+                indent_all_by(2, pkg.get_description().unwrap_or(""))
+            );
 
             let mut available_scriptlets: Vec<String> = vec![];
             for (scriptlet_name, func) in &SCRIPTLET_METHODS {
-                match func(&pkg) {
-                    Ok(_) => {
-                        available_scriptlets.push(scriptlet_name.to_string());
-                        //println!("{}: |\n{}", scriptlet_name, indent_all_by(2, scriptlet.script));
-                    },
-                    Err(_) => {}
-                };
+                if func(&pkg).is_ok() {
+                    available_scriptlets.push(scriptlet_name.to_string());
+                }
             }
             println!("Scriptlets: [{}]", available_scriptlets.join(", "));
         }
@@ -236,13 +267,16 @@ fn main() {
                 }
             };
 
-            for (scriptlet_name, func) in SCRIPTLET_METHODS.into_iter().filter(|(key, _)| cmd.scriptlets.is_empty() || cmd.scriptlets.contains(&key.to_string())) {
-                match func(&pkg.metadata) {
-                    Ok(scriptlet) => {
-                        println!("{}: |\n{}", scriptlet_name, indent_all_by(2, scriptlet.script));
-                    },
-                    Err(_) => {}
-                };
+            for (scriptlet_name, func) in SCRIPTLET_METHODS.into_iter().filter(|(key, _)| {
+                cmd.scriptlets.is_empty() || cmd.scriptlets.contains(&key.to_string())
+            }) {
+                if let Ok(scriptlet) = func(&pkg.metadata) {
+                    println!(
+                        "{}: |\n{}",
+                        scriptlet_name,
+                        indent_all_by(2, scriptlet.script)
+                    );
+                }
             }
         }
     }
